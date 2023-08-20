b5833a72420b1966f00e0224bc624dc513c2a9b3
Kartik <kartik.mathur10@gmail.com>
2023-08-20 20:22:45 +0530

    Recursion

---
+++ Lecture-18-Recursion/NStairs.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+int nStairs(int n) {
+	// base case
+	if (n == 0) {
+		return 1;
+	}
+
+	if (n < 0) {
+		return 0;
+	}
+
+	// recursive case
+	return nStairs(n - 1) + nStairs(n - 2) + nStairs(n - 3);
+}
+
+int nStairs1(int n, int k) {
+	// base case
+	if (n == 0) {
+		return 1;
+	}
+
+	if (n < 0) {
+		return 0;
+	}
+
+	// recursive case
+	int ans = 0;
+	for (int i = 1; i <= k; ++i)
+	{
+		ans += nStairs1(n - i, k);
+	}
+	return ans;
+}
+
+int main() {
+
+	cout << nStairs1(5, 3) << endl;
+	cout << nStairs(5) << endl;
+	cout << nStairs(4) << endl;
+	cout << nStairs(3) << endl;
+	cout << nStairs(2) << endl;
+	cout << nStairs(1) << endl;
+	cout << nStairs(0) << endl;
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
